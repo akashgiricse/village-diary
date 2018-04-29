c761a652f842cc7b168473f223e5408eee06ea7f
akashgiricse <akashgiricse@gmail.com>
2018-04-29 09:55:24 +0530

    Customized admin panel

--- village_diary/diary/admin.py
+++ village_diary/diary/admin.py
@@ -3,5 +3,17 @@ from .models import Diary, dataEntryEmployee
 # Register your models here.
 
 
-admin.site.register(Diary)
-admin.site.register(dataEntryEmployee)
+class DiaryAdmin(admin.ModelAdmin):
+    model = Diary
+    list_display = ['villageName', 'villageCode', 'connectivity', 'allotted']
+    list_filter = ['connectivity', 'allotted']
+    search_fields = ['villageName', 'villageCode', 'gataNumber']
+
+
+class dataEntryEmployeeAdmin(admin.ModelAdmin):
+    model = dataEntryEmployee
+    search_fields = ['employeeCodes']
+
+
+admin.site.register(Diary, DiaryAdmin)
+admin.site.register(dataEntryEmployee, dataEntryEmployeeAdmin)
